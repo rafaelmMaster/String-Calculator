9b9c90d78a0e5f05b42d9720cc48969f26c79470
rafaelmMaster <motyl93@hotmail.com>
2014-10-16 23:23:01 +0000

    refactoring toInt method

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -9,10 +9,14 @@ public class Calculator {
 		}
 		else if (text.contains(",")){
 			String[] numbers = text.split(",");
-			return Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]);
+			return toInt(numbers[0]) + toInt(numbers[1]);
 		}
 		else {
 			return 1;
 		}
+	}
+
+	private static int toInt(String number) {
+		return Integer.parseInt(number);
 	}
 }
