91895e0365d1842cc7fa7970aec8e6aca788bb2f
rafaelmMaster <motyl93@hotmail.com>
2014-10-16 22:24:52 +0000

    Calculator with empty string

---
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -0,0 +1,18 @@
+package is.ru.stringcalculator;
+
+public class Calculator {
+	
+	public static int add(String text) {
+		
+		if (text.isEmpty()) {
+			return 0;
+		}
+		else if (text.contains(",")){
+			String[] numbers = text.split(",");
+			return Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]);
+		}
+		else {
+			return 1;
+		}
+	}
+}
