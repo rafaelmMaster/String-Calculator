caaeec9dc53907c172fa09f8c8b103153a6f3374
rafaelmMaster <motyl93@hotmail.com>
2014-10-16 23:33:49 +0000

    multiple numbers method added

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -8,8 +8,7 @@ public class Calculator {
 			return 0;
 		}
 		else if (text.contains(",")){
-			String[] numbers = text.split(",");
-			return toInt(numbers[0]) + toInt(numbers[1]);
+			return sum(splitNumbers(text));
 		}
 		else {
 			return 1;
@@ -18,5 +17,17 @@ public class Calculator {
 
 	private static int toInt(String number) {
 		return Integer.parseInt(number);
+	}
+
+	private static String[] splitNumbers(String numbers){
+		return numbers.split(",");
+	}
+	
+	private static int sum(String[] numbers){
+		int total = 0;
+		for(String number : numbers) {
+			total += toInt(number);
+		}
+		return total;
 	}
 }
