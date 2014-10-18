5b3cefd6d197cb719182577cb20d0dd8382e51ac
rafaelmMaster <motyl93@hotmail.com>
2014-10-18 17:46:36 +0000

    Test for multiple delimiters

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -77,4 +77,9 @@ public class CalculatorTest {
 		assertEquals(6, Calculator.add("//[***]\n1***2***3"));
 	}
 
+	@Test
+	public void testAnyKindOfDelimiters() {
+		assertEquals(6, Calculator.add("//[*][%]\n1*2%3"));
+	}
+
 }
